55cd3c6222c6c64b383f58b21b12c142b0a5f7f6
Joaquin <joaquin.alonso.astudillo@gmail.com>
2021-08-20 15:17:59 +0200

    Add finished dropdown component with useRef

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -16,10 +18,34 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div>
-      <Accordion items={items} />
+      <button onClick={() => setShowDropdown(!showDropdown)}>toggle</button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
