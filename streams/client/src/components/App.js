adca6df631fef04b24447373f06009fb1490c12a
Joaquin <joaquin.alonso.astudillo@gmail.com>
2021-08-24 16:45:24 +0200

    Add streams app with React Router Dom

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "../components/streams/StreamCreate";
+import StreamEdit from "../components/streams/StreamEdit";
+import StreamDelete from "../components/streams/StreamDelete";
+import StreamList from "../components/streams/StreamList";
+import StreamShow from "../components/streams/StreamShow";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
