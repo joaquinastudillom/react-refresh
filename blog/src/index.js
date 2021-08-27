30f673d644b1e217a36c905c7e3a9854f98ec209
Joaquin <joaquin.alonso.astudillo@gmail.com>
2021-08-27 09:56:08 +0200

    add react redux example in project blog

---
+++ blog/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+
+import App from "./components/App";
+import reducers from "./reducers/index";
+
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
